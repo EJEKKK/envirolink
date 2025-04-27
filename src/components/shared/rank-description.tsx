bb4cfdc3815b6a904fbef0cbed810c114574e156
Jeyeemm <86698593+Jeyeemm@users.noreply.github.com>
2025-04-27 21:20:57 +0800

    refactor: change website link for sharing to facebook

--- src/components/shared/rank-description.tsx
+++ src/components/shared/rank-description.tsx
@@ -12,8 +12,17 @@ import {
 } from "../ui/dialog";
 import { ScrollArea, ScrollBar } from "../ui/scroll-area";
 
-import { doc, getDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { Loader2 } from "lucide-react";
+import { formatCompactNumber, rankDescriptionConverter } from "@/lib/utils";
+import type { RankDescription } from "@/types";
 
 interface RankDescriptionDialogProps {
   open: boolean;
@@ -24,32 +33,29 @@ export default function RankDescriptionDialog({
   open,
   onOpenChange,
 }: RankDescriptionDialogProps) {
-  const [rankPoints, setRankPoints] = React.useState<number[]>([]);
+  const [ranks, setRanks] = React.useState<RankDescription[]>([]);
 
   React.useEffect(() => {
-    async function fetchRankPoints() {
-      const rankPoints: Awaited<number[]> = (await Promise.all([
-        (await getDoc(doc(db, "rankDescription", "silver")))?.data()
-          ?.points as Awaited<number>,
-        (await getDoc(doc(db, "rankDescription", "gold")))?.data()
-          ?.points as Awaited<number>,
-        (await getDoc(doc(db, "rankDescription", "platinum")))?.data()
-          ?.points as Awaited<number>,
-        (await getDoc(doc(db, "rankDescription", "diamond")))?.data()
-          ?.points as Awaited<number>,
-      ])) as number[];
+    const rankRef = collection(db, "rankDescription").withConverter(
+      rankDescriptionConverter,
+    );
+    const q = query(rankRef, orderBy("createdAt", "asc"));
+    const unsubRankDescriptions = onSnapshot(q, (snapshot) => {
+      const rankDescriptions: RankDescription[] = [];
 
-      if (rankPoints.length > 0) {
-        setRankPoints(rankPoints);
+      for (const doc of snapshot.docs) {
+        rankDescriptions.push({ ...doc.data(), id: doc.id });
       }
-    }
 
-    void fetchRankPoints();
+      setRanks(rankDescriptions);
+    });
+
+    return () => unsubRankDescriptions();
   }, []);
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-4xl">
+      <DialogContent className="w-fit sm:max-w-4xl">
         <DialogHeader>
           <DialogTitle>Rank Description</DialogTitle>
           <DialogDescription>
@@ -58,69 +64,33 @@ export default function RankDescriptionDialog({
         </DialogHeader>
         <ScrollArea className="h-fit min-w-full">
           <div className="flex items-center justify-center gap-4 text-center">
-            {rankPoints.length === 0 ? (
+            {ranks.length === 0 ? (
               <Loader2 className="animate-spin" />
             ) : (
-              <>
-                <div className="flex flex-col items-center justify-center gap-2">
+              ranks.map((rank, index) => (
+                <div
+                  key={rank.id}
+                  className="flex flex-col items-center justify-center gap-2"
+                >
                   <div className="relative w-40">
                     <AspectRatio ratio={1 / 1}>
-                      <img src="/badges/BRONZE.png" alt="frame tier" />
+                      <img src={rank.image} alt={rank.name} />
                     </AspectRatio>
                   </div>
-                  <p className="text-sm font-bold">Bronze</p>
+                  <p className="text-sm font-bold">{rank.name}</p>
                   <small className="text-muted-foreground">
-                    0 - {(rankPoints[0] as number) - 1} points
-                  </small>
-                </div>
-                <div className="flex flex-col items-center justify-center gap-2">
-                  <div className="relative w-40">
-                    <AspectRatio ratio={1 / 1}>
-                      <img src="/badges/SILVER.png" alt="frame tier" />
-                    </AspectRatio>
-                  </div>
-                  <p className="text-sm font-bold">Silver</p>
-                  <small className="text-muted-foreground">
-                    {rankPoints[0] as number} - {(rankPoints[1] as number) - 1}{" "}
+                    {ranks.length - 1 > index
+                      ? `${formatCompactNumber(rank.points)} -`
+                      : null}
+                    {ranks.length - 1 === index
+                      ? `${formatCompactNumber(rank.points)}+`
+                      : formatCompactNumber(
+                          (ranks[index + 1]?.points as number) - 1,
+                        )}{" "}
                     points
                   </small>
                 </div>
-                <div className="flex flex-col items-center justify-center gap-2">
-                  <div className="relative w-40">
-                    <AspectRatio ratio={1 / 1}>
-                      <img src="/badges/GOLD.png" alt="frame tier" />
-                    </AspectRatio>
-                  </div>
-                  <p className="text-sm font-bold">Gold</p>
-                  <small className="text-muted-foreground">
-                    {rankPoints[1] as number} - {(rankPoints[2] as number) - 1}{" "}
-                    points
-                  </small>
-                </div>
-                <div className="flex flex-col items-center justify-center gap-2">
-                  <div className="relative w-40">
-                    <AspectRatio ratio={1 / 1}>
-                      <img src="/badges/PLATINUM.png" alt="frame tier" />
-                    </AspectRatio>
-                  </div>
-                  <p className="text-sm font-bold">Platinum</p>
-                  <small className="text-muted-foreground">
-                    {rankPoints[2] as number} - {(rankPoints[3] as number) - 1}{" "}
-                    points
-                  </small>
-                </div>
-                <div className="flex flex-col items-center justify-center gap-2">
-                  <div className="relative w-40">
-                    <AspectRatio ratio={1 / 1}>
-                      <img src="/badges/DIAMOND.png" alt="frame tier" />
-                    </AspectRatio>
-                  </div>
-                  <p className="text-sm font-bold">Diamond</p>
-                  <small className="text-muted-foreground">
-                    {rankPoints[3] as number}+ points
-                  </small>
-                </div>
-              </>
+              ))
             )}
           </div>
           <ScrollBar orientation="horizontal" />
