35b2433f6cda75007dfac288d79cf302e6acd1ed
Jeyeemm <86698593+Jeyeemm@users.noreply.github.com>
2025-06-02 14:06:58 +0800

    fix(admin): update rank ordering and image update logic

--- src/app/admin/rank-description/_components/edit-rank-dialog-form.tsx
+++ src/app/admin/rank-description/_components/edit-rank-dialog-form.tsx
@@ -3,32 +3,32 @@ import * as React from "react";
 
 import { Button } from "@/components/ui/button";
 import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
 } from "@/components/ui/dialog";
 import {
-  DropZoneArea,
-  Dropzone,
-  DropzoneFileList,
-  DropzoneFileListItem,
-  DropzoneMessage,
-  DropzoneRemoveFile,
-  DropzoneTrigger,
-  useDropzone,
+	DropZoneArea,
+	Dropzone,
+	DropzoneFileList,
+	DropzoneFileListItem,
+	DropzoneMessage,
+	DropzoneRemoveFile,
+	DropzoneTrigger,
+	useDropzone,
 } from "@/components/ui/dropzone";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
@@ -36,245 +36,245 @@ import { db, storage } from "@/config/firebase";
 import { useIsClient } from "@/hooks/use-is-client";
 import type { RankDescription } from "@/types";
 import {
-  type EditRankDescriptionFormSchema,
-  editRankDescriptionFormSchema,
+	type EditRankDescriptionFormSchema,
+	editRankDescriptionFormSchema,
 } from "../_lib/validations";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
-  addDoc,
-  collection,
-  doc,
-  serverTimestamp,
-  updateDoc,
+	addDoc,
+	collection,
+	doc,
+	serverTimestamp,
+	updateDoc,
 } from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import {
-  CloudUploadIcon,
-  Edit2Icon,
-  Loader2Icon,
-  Trash2Icon,
+	CloudUploadIcon,
+	Edit2Icon,
+	Loader2Icon,
+	Trash2Icon,
 } from "lucide-react";
 import { useForm, useWatch } from "react-hook-form";
 import { Skeleton } from "@/components/ui/skeleton";
 
 interface EditRankDescriptionProps {
-  rank: RankDescription;
+	rank: RankDescription;
 }
 
 export default function EditRankDialog({ rank }: EditRankDescriptionProps) {
-  const form = useForm<EditRankDescriptionFormSchema>({
-    resolver: zodResolver(editRankDescriptionFormSchema),
-    defaultValues: {
-      id: rank.id ?? "",
-      name: rank.name ?? "",
-      points: rank.points ?? 0,
-      image: rank.image ?? [],
-      file: [],
-    },
-  });
-  const imageFile = useWatch({ control: form.control, name: "file" });
+	const form = useForm<EditRankDescriptionFormSchema>({
+		resolver: zodResolver(editRankDescriptionFormSchema),
+		defaultValues: {
+			id: rank.id ?? "",
+			name: rank.name ?? "",
+			points: rank.points ?? 0,
+			image: rank.image ?? [],
+			file: [],
+		},
+	});
+	const imageFile = useWatch({ control: form.control, name: "file" });
 
-  const [isEditRankDialogOpen, setIsEditRankDialogOpen] = React.useState(false);
-  const [isUpdating, setIsUpdating] = React.useState(false);
-  const isClient = useIsClient();
+	const [isEditRankDialogOpen, setIsEditRankDialogOpen] = React.useState(false);
+	const [isUpdating, setIsUpdating] = React.useState(false);
+	const isClient = useIsClient();
 
-  const dropzone = useDropzone({
-    onDropFile: async (file) => {
-      form.setValue("file", [file]);
-      await new Promise((resolve) => setTimeout(resolve, 500));
-      return {
-        status: "success",
-        result: URL.createObjectURL(file),
-      };
-    },
-    validation: {
-      accept: {
-        "image/*": [".png", ".jpg", ".jpeg"],
-      },
-      maxSize: 1024 * 1024 * 5, // 5MB
-      maxFiles: 1,
-    },
-  });
+	const dropzone = useDropzone({
+		onDropFile: async (file) => {
+			form.setValue("file", [file]);
+			await new Promise((resolve) => setTimeout(resolve, 500));
+			return {
+				status: "success",
+				result: URL.createObjectURL(file),
+			};
+		},
+		validation: {
+			accept: {
+				"image/*": [".png", ".jpg", ".jpeg"],
+			},
+			maxSize: 1024 * 1024 * 5, // 5MB
+			maxFiles: 1,
+		},
+	});
 
-  const handleOnUpdateRank = async (values: EditRankDescriptionFormSchema) => {
-    setIsUpdating(true);
-    const rankRef = doc(db, "rankDescription", rank.id);
-    const rankImageRef = ref(storage, `ranks/${values.name}-${Date.now()}`);
+	const handleOnUpdateRank = async (values: EditRankDescriptionFormSchema) => {
+		setIsUpdating(true);
+		const rankRef = doc(db, "rankDescription", rank.id);
+		const rankImageRef = ref(storage, `ranks/${values.name}-${Date.now()}`);
 
-    await uploadBytes(rankImageRef, imageFile[0] as File).then(
-      async (snapshot) => {
-        const fileURL = await getDownloadURL(snapshot.ref);
+		await uploadBytes(rankImageRef, imageFile[0] as File).then(
+			async (snapshot) => {
+				const fileURL = await getDownloadURL(snapshot.ref);
 
-        await updateDoc(rankRef, {
-          name: values.name,
-          points: values.points,
-          image: fileURL,
-        });
-        form.reset();
-        setIsUpdating(false);
-        setIsEditRankDialogOpen(false);
-      },
-    );
-  };
+				await updateDoc(rankRef, {
+					name: values.name,
+					points: values.points,
+					image: values.file.length >= 1 ? fileURL : values.image,
+				});
+				form.reset();
+				setIsUpdating(false);
+				setIsEditRankDialogOpen(false);
+			},
+		);
+	};
 
-  return (
-    <Dialog
-      open={isEditRankDialogOpen}
-      onOpenChange={(open) => {
-        if (!open) {
-          form.reset();
-          dropzone.fileStatuses.length = 0;
-        }
+	return (
+		<Dialog
+			open={isEditRankDialogOpen}
+			onOpenChange={(open) => {
+				if (!open) {
+					form.reset();
+					dropzone.fileStatuses.length = 0;
+				}
 
-        setIsEditRankDialogOpen(open);
-      }}
-    >
-      <DialogTrigger asChild>
-        <Button className="w-full">
-          <Edit2Icon /> Edit Rank
-        </Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Edit Rank</DialogTitle>
-          <DialogDescription>
-            Edit rank description for your application.
-          </DialogDescription>
-        </DialogHeader>
-        <ScrollArea className="h-full max-h-96">
-          <Form {...form}>
-            <form className="space-y-4">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Name</FormLabel>
-                    <FormControl>
-                      <Input {...field} placeholder="Enter rank name" />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+				setIsEditRankDialogOpen(open);
+			}}
+		>
+			<DialogTrigger asChild>
+				<Button className="w-full">
+					<Edit2Icon /> Edit Rank
+				</Button>
+			</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Edit Rank</DialogTitle>
+					<DialogDescription>
+						Edit rank description for your application.
+					</DialogDescription>
+				</DialogHeader>
+				<ScrollArea className="h-full max-h-96">
+					<Form {...form}>
+						<form className="space-y-4">
+							<FormField
+								control={form.control}
+								name="name"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Name</FormLabel>
+										<FormControl>
+											<Input {...field} placeholder="Enter rank name" />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
 
-              <FormField
-                control={form.control}
-                name="points"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Points</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="number"
-                        min={1}
-                        {...field}
-                        placeholder="Enter rank points"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+							<FormField
+								control={form.control}
+								name="points"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Points</FormLabel>
+										<FormControl>
+											<Input
+												type="number"
+												min={1}
+												{...field}
+												placeholder="Enter rank points"
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
 
-              <FormField
-                control={form.control}
-                name="image"
-                render={() => (
-                  <FormItem>
-                    <FormLabel>Image</FormLabel>
-                    <FormControl>
-                      <Dropzone {...dropzone}>
-                        <DropzoneMessage />
-                        <DropZoneArea>
-                          <DropzoneTrigger className="flex flex-col items-center gap-4 bg-transparent p-10 text-center text-sm">
-                            <CloudUploadIcon className="size-8" />
-                            <div>
-                              <p className="font-semibold">
-                                Upload listing images
-                              </p>
-                              <p className="text-muted-foreground text-sm">
-                                Click here or drag and drop to upload
-                              </p>
-                            </div>
-                          </DropzoneTrigger>
-                        </DropZoneArea>
+							<FormField
+								control={form.control}
+								name="image"
+								render={() => (
+									<FormItem>
+										<FormLabel>Image</FormLabel>
+										<FormControl>
+											<Dropzone {...dropzone}>
+												<DropzoneMessage />
+												<DropZoneArea>
+													<DropzoneTrigger className="flex flex-col items-center gap-4 bg-transparent p-10 text-center text-sm">
+														<CloudUploadIcon className="size-8" />
+														<div>
+															<p className="font-semibold">
+																Upload listing images
+															</p>
+															<p className="text-muted-foreground text-sm">
+																Click here or drag and drop to upload
+															</p>
+														</div>
+													</DropzoneTrigger>
+												</DropZoneArea>
 
-                        <DropzoneFileList className="flex w-full gap-3 p-0">
-                          {dropzone.fileStatuses.map((file) => (
-                            <DropzoneFileListItem
-                              className="bg-secondary relative flex w-40 flex-col overflow-hidden rounded-md p-0 shadow-sm"
-                              key={file.id}
-                              file={file}
-                            >
-                              {file.status === "pending" && (
-                                <Skeleton className="bg-background/20 aspect-video animate-pulse" />
-                              )}
-                              {file.status === "success" && (
-                                // eslint-disable-next-line @next/next/no-img-element
-                                <img
-                                  src={file.result}
-                                  alt={`uploaded-${file.fileName}`}
-                                  className="object-cover"
-                                />
-                              )}
-                              <div className="flex items-center justify-between p-2 pl-4">
-                                <div className="min-w-0">
-                                  <p className="truncate text-sm">
-                                    {file.fileName}
-                                  </p>
-                                  <p className="text-muted-foreground text-xs">
-                                    {(file.file.size / (1024 * 1024)).toFixed(
-                                      2,
-                                    )}{" "}
-                                    MB
-                                  </p>
-                                </div>
-                                <DropzoneRemoveFile
-                                  variant="ghost"
-                                  className="shrink-0 hover:outline"
-                                  onPointerDown={() => {
-                                    form.setValue(
-                                      "file",
-                                      imageFile.filter((f) => f !== file.file),
-                                    );
-                                  }}
-                                >
-                                  <Trash2Icon className="size-4" />
-                                </DropzoneRemoveFile>
-                              </div>
-                            </DropzoneFileListItem>
-                          ))}
-                        </DropzoneFileList>
-                      </Dropzone>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </form>
-          </Form>
-        </ScrollArea>
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button variant="ghost" disabled={isUpdating}>
-              Close
-            </Button>
-          </DialogClose>
-          <Button
-            type="button"
-            onClick={form.handleSubmit(handleOnUpdateRank)}
-            disabled={
-              isUpdating ||
-              (!form.formState.isDirty && form.watch("file").length === 0)
-            }
-          >
-            {isUpdating && <Loader2Icon className="animate-spin" />} Save
-            changes
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  );
+												<DropzoneFileList className="flex w-full gap-3 p-0">
+													{dropzone.fileStatuses.map((file) => (
+														<DropzoneFileListItem
+															className="bg-secondary relative flex w-40 flex-col overflow-hidden rounded-md p-0 shadow-sm"
+															key={file.id}
+															file={file}
+														>
+															{file.status === "pending" && (
+																<Skeleton className="bg-background/20 aspect-video animate-pulse" />
+															)}
+															{file.status === "success" && (
+																// eslint-disable-next-line @next/next/no-img-element
+																<img
+																	src={file.result}
+																	alt={`uploaded-${file.fileName}`}
+																	className="object-cover"
+																/>
+															)}
+															<div className="flex items-center justify-between p-2 pl-4">
+																<div className="min-w-0">
+																	<p className="truncate text-sm">
+																		{file.fileName}
+																	</p>
+																	<p className="text-muted-foreground text-xs">
+																		{(file.file.size / (1024 * 1024)).toFixed(
+																			2,
+																		)}{" "}
+																		MB
+																	</p>
+																</div>
+																<DropzoneRemoveFile
+																	variant="ghost"
+																	className="shrink-0 hover:outline"
+																	onPointerDown={() => {
+																		form.setValue(
+																			"file",
+																			imageFile.filter((f) => f !== file.file),
+																		);
+																	}}
+																>
+																	<Trash2Icon className="size-4" />
+																</DropzoneRemoveFile>
+															</div>
+														</DropzoneFileListItem>
+													))}
+												</DropzoneFileList>
+											</Dropzone>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+						</form>
+					</Form>
+				</ScrollArea>
+				<DialogFooter>
+					<DialogClose asChild>
+						<Button variant="ghost" disabled={isUpdating}>
+							Close
+						</Button>
+					</DialogClose>
+					<Button
+						type="button"
+						onClick={form.handleSubmit(handleOnUpdateRank)}
+						disabled={
+							isUpdating ||
+							(!form.formState.isDirty && form.watch("file").length === 0)
+						}
+					>
+						{isUpdating && <Loader2Icon className="animate-spin" />} Save
+						changes
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
 }
--- src/app/admin/rank-description/page.tsx
+++ src/app/admin/rank-description/page.tsx
@@ -17,93 +17,89 @@ import { Loader2Icon, PlusCircleIcon, Trash2Icon } from "lucide-react";
 import DeleteRankDialog from "./_components/delete-rank-dialog";
 
 export default function RankDescriptionPage() {
-  const [ranks, setRanks] = React.useState<RankDescription[]>([]);
-  const [isFetching, setIsFetching] = React.useState(false);
-  const isClient = useIsClient();
+	const [ranks, setRanks] = React.useState<RankDescription[]>([]);
+	const [isFetching, setIsFetching] = React.useState(false);
+	const isClient = useIsClient();
 
-  React.useEffect(() => {
-    setIsFetching(true);
-    const ranksRef = collection(db, "rankDescription").withConverter(
-      rankDescriptionConverter,
-    );
-    const q = query(
-      ranksRef,
-      orderBy("createdAt", "desc"),
-      orderBy("points", "desc"),
-    );
+	React.useEffect(() => {
+		setIsFetching(true);
+		const ranksRef = collection(db, "rankDescription").withConverter(
+			rankDescriptionConverter,
+		);
+		const q = query(ranksRef, orderBy("points", "asc"));
 
-    const unsubRanks = onSnapshot(q, (snapshot) => {
-      const ranksData: RankDescription[] = [];
+		const unsubRanks = onSnapshot(q, (snapshot) => {
+			const ranksData: RankDescription[] = [];
 
-      for (const doc of snapshot.docs) {
-        ranksData.push({ ...doc.data(), id: doc.id });
-      }
+			for (const doc of snapshot.docs) {
+				ranksData.push({ ...doc.data(), id: doc.id });
+			}
 
-      setRanks(ranksData);
-      setIsFetching(false);
-    });
+			setRanks(ranksData);
+			setIsFetching(false);
+		});
 
-    return () => {
-      unsubRanks();
-    };
-  }, []);
+		return () => {
+			unsubRanks();
+		};
+	}, []);
 
-  if (!isClient) return null;
+	if (!isClient) return null;
 
-  return (
-    <>
-      <header className="flex h-16 shrink-0 items-center justify-between gap-2 px-4 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-        <div className="flex items-center gap-2">
-          <SidebarTrigger className="-ml-1" />
-          <h4 className="text-lg font-semibold">Rank Description</h4>
-        </div>
+	return (
+		<>
+			<header className="flex h-16 shrink-0 items-center justify-between gap-2 px-4 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+				<div className="flex items-center gap-2">
+					<SidebarTrigger className="-ml-1" />
+					<h4 className="text-lg font-semibold">Rank Description</h4>
+				</div>
 
-        <CreateRankDialog />
-      </header>
-      <main className="mt-4 p-4">
-        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
-          {!isFetching ? (
-            ranks.length >= 1 ? (
-              ranks.map((rank) => (
-                <Card key={rank.id}>
-                  <CardContent className="flex flex-col items-center gap-4">
-                    <img className="w-20" src={rank.image} alt={rank.name} />
-                    <div className="text-center">
-                      <p>{rank.name}</p>
-                      <p className="text-primary">
-                        {formatCompactNumber(rank.points)} points
-                      </p>
-                    </div>
-                  </CardContent>
-                  <CardFooter className="flex w-full flex-col gap-2">
-                    <EditRankDialog rank={rank} />
-                    <DeleteRankDialog rankId={rank.id} />
-                  </CardFooter>
-                </Card>
-              ))
-            ) : (
-              <div className="text-muted-foreground flex flex-col items-center justify-center gap-4 rounded-md border border-dashed p-4 sm:col-span-2 md:col-span-3">
-                <Image
-                  className="w-40"
-                  src="/illustrations/create-empty-state.svg"
-                  alt="create rank"
-                  priority
-                  height={40}
-                  width={40}
-                />
-                <h4 className="text-lg font-semibold">
-                  No ranks have been created yet
-                </h4>
-                <CreateRankDialog />
-              </div>
-            )
-          ) : (
-            <div className="flex items-center justify-center p-4 sm:col-span-2 md:col-span-3">
-              <Loader2Icon className="text-primary animate-spin" />
-            </div>
-          )}
-        </div>
-      </main>
-    </>
-  );
+				<CreateRankDialog />
+			</header>
+			<main className="mt-4 p-4">
+				<div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
+					{!isFetching ? (
+						ranks.length >= 1 ? (
+							ranks.map((rank) => (
+								<Card key={rank.id}>
+									<CardContent className="flex flex-col items-center gap-4">
+										<img className="w-20" src={rank.image} alt={rank.name} />
+										<div className="text-center">
+											<p>{rank.name}</p>
+											<p className="text-primary">
+												{formatCompactNumber(rank.points)} points
+											</p>
+										</div>
+									</CardContent>
+									<CardFooter className="flex w-full flex-col gap-2">
+										<EditRankDialog rank={rank} />
+										<DeleteRankDialog rankId={rank.id} />
+									</CardFooter>
+								</Card>
+							))
+						) : (
+							<div className="text-muted-foreground flex flex-col items-center justify-center gap-4 rounded-md border border-dashed p-4 sm:col-span-2 md:col-span-3">
+								<Image
+									className="w-40"
+									src="/illustrations/create-empty-state.svg"
+									alt="create rank"
+									priority
+									height={40}
+									width={40}
+								/>
+								<h4 className="text-lg font-semibold">
+									No ranks have been created yet
+								</h4>
+								<CreateRankDialog />
+							</div>
+						)
+					) : (
+						<div className="flex items-center justify-center p-4 sm:col-span-2 md:col-span-3">
+							<Loader2Icon className="text-primary animate-spin" />
+						</div>
+					)}
+				</div>
+			</main>
+		</>
+	);
 }
